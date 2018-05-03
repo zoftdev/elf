670897d676026e4735985bf2ca072dfe375e674a
Thisadee Preeyawongsakul <thisadee_pre@truecorp.co.th>
2018-05-03 10:23:59 +0700

    [FEATURE] add new test project

---
+++ kafka-test/src/test/java/com/example/kafkatest/KafkaTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kafkatest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
